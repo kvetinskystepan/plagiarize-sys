b27f999b5aa682c3eb13c3f067a23d2c722f4d44
kvetinskystepan <stepan.kvetinsky@gmail.com>
2022-08-04 02:08:16 +0200

    Adding ProxySystem

---
+++ proxy-projects/proxy/src/main/java/proxysystem/messages/PrivateMessages.java
@@ -0,0 +1,12 @@
+package com.thenarbox.proxysystem.messages;
+import net.md_5.bungee.api.plugin.Listener;
+
+public class PrivateMessages implements Listener {
+    public PrivateMessages() {
+
+    }
+
+
+
+
+}
---
+++ proxy-projects/proxy/src/main/java/proxysystem/motd/Motd.java
@@ -0,0 +1,33 @@
+package com.thenarbox.proxysystem.motd;
+
+import com.thenarbox.proxysystem.ProxySystem;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.event.ProxyPingEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+import java.io.IOException;
+
+public class Motd implements Listener {
+
+    private static Motd instance;
+    public Motd() {
+
+    }
+
+    public static Motd getInstance() {
+        return instance;
+    }
+
+
+    @EventHandler(priority = 64)
+    public void onProxyPing(ProxyPingEvent e) throws IOException {
+        ServerPing ping = e.getResponse();
+        getInstance();
+        String mod1 = ChatColor.translateAlternateColorCodes('&', "&6&lMejs.cz &7| &fVerze: &61.19");
+        String mod2 = ChatColor.WHITE + "Server je v rezimu " + ChatColor.RED + "UDRZBY";
+        ping.setDescription(String.valueOf(String.valueOf(String.valueOf(mod1))) + "\n" + mod2);
+        e.setResponse(ping);
+    }
+}

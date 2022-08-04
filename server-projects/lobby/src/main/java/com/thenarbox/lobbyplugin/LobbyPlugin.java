c0aa346e8bdaa299912625d95884facc67ad64e0
kvetinskystepan <stepan.kvetinsky@gmail.com>
2022-08-04 23:07:10 +0200

    Proxy

--- server-projects/lobby/src/main/java/com/thenarbox/lobbyplugin/LobbyPlugin.java
+++ server-projects/lobby/src/main/java/com/thenarbox/lobbyplugin/LobbyPlugin.java
@@ -1,5 +1,6 @@
 package com.thenarbox.lobbyplugin;
 
+import com.destroystokyo.paper.event.block.TNTPrimeEvent;
 import com.thenarbox.api.Standards;
 import com.thenarbox.lobbyplugin.extenders.DoubleJump;
 import com.thenarbox.lobbyplugin.listeners.CommandMechanic;
@@ -7,15 +8,17 @@ import lombok.extern.log4j.Log4j2;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.block.BlockSpreadEvent;
-import org.bukkit.event.block.LeavesDecayEvent;
+import org.bukkit.event.block.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -126,6 +129,14 @@ public class LobbyPlugin extends JavaPlugin implements Listener {
     @EventHandler
     public void noPvP(EntityDamageByEntityEvent e){
         e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBoom(final EntityExplodeEvent e) {
+        final Entity entity = e.getEntity();
+        if (entity != null && entity instanceof TNTPrimed) {
+            e.blockList().clear();
+        }
     }
 
 }

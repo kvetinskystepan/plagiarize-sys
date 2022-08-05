651beaf39be9dfac3a9fe9198d98cfd01c6eefd7
kvetinskystepan <stepan.kvetinsky@gmail.com>
2022-08-05 02:51:50 +0200

    System

---
+++ api-proxy/src/main/java/com/thenarbox/api/ChatNotice.java
@@ -0,0 +1,31 @@
+package com.thenarbox.api;
+
+import lombok.extern.log4j.Log4j2;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.awt.*;
+
+@Log4j2(topic = "ChatNotice")
+public class ChatNotice {
+
+    public ChatNotice() {
+    }
+
+    public static void info(ProxiedPlayer player, Component component){
+        player.sendMessage(ChatColor.GOLD + "✎ "  + ChatColor.GRAY + "| " + ChatColor.WHITE + PlainTextComponentSerializer.plainText().serialize(component));
+    }
+
+    public static void error(ProxiedPlayer player, Component component){
+        player.sendMessage(ChatColor.DARK_RED + "✖ " + ChatColor.GRAY + "| " + ChatColor.WHITE + PlainTextComponentSerializer.plainText().serialize(component));
+    }
+
+    public static void success(ProxiedPlayer player, Component component){
+        player.sendMessage(ChatColor.GREEN + "✔ " + ChatColor.GRAY + "| " + ChatColor.WHITE + PlainTextComponentSerializer.plainText().serialize(component));
+    }
+
+    public static void warning(ProxiedPlayer player, Component component){
+        player.sendMessage(ChatColor.YELLOW + "⚠ " + ChatColor.GRAY + "| " + ChatColor.WHITE + PlainTextComponentSerializer.plainText().serialize(component));
+    }
+
+}

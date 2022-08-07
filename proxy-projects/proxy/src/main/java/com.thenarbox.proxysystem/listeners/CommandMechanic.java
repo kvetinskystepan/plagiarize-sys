9f486946d4445cdc6eb6076a71c090afd352e488
kvetinskystepan <stepan.kvetinsky@gmail.com>
2022-08-07 19:53:43 +0200

    System

--- proxy-projects/proxy/src/main/java/com.thenarbox.proxysystem/listeners/CommandMechanic.java
+++ proxy-projects/proxy/src/main/java/com.thenarbox.proxysystem/listeners/CommandMechanic.java
@@ -16,12 +16,30 @@ public class CommandMechanic implements Listener {
 
     public static void Commands(){
         {
+            ProxyServer.getInstance().getPluginManager().registerCommand(ProxySystem.getStaticInstance(), new Command("build") {
+                @Override
+                public void execute(CommandSender sender, String[] args) {
+                    ProxiedPlayer player = (ProxiedPlayer) sender;
+                    if (!player.hasPermission("proxyserver.build.join")){
+                        ChatNotice.error(player, Component.text("Minimální hodnost pro připojení na tento server je Builder."));
+                        return;
+                    }
+                    if(!player.getServer().getInfo().getMotd().equals("Build")){
+                        ChatNotice.success(player, Component.text("Připojuješ se na BuildServer."));
+                        player.connect(ProxyServer.getInstance().getServerInfo("Build"));
+                        return;
+                    }
+                    ChatNotice.error(player, Component.text("Jsi již na serveru pro stavění."));
+                }
+            });
+        }
+        {
             ProxyServer.getInstance().getPluginManager().registerCommand(ProxySystem.getStaticInstance(), new Command("lobby") {
                 @Override
                 public void execute(CommandSender sender, String[] args) {
                     ProxiedPlayer player = (ProxiedPlayer) sender;
                     if(!player.getServer().getInfo().getMotd().equals("Lobby-1")){
-                        ChatNotice.success(player, Component.text("Přesunušeš se na Lobby."));
+                        ChatNotice.success(player, Component.text("Připojuješ se na Lobby."));
                         player.connect(ProxyServer.getInstance().getServerInfo("Lobby-1"));
                         return;
                     }
@@ -35,7 +53,7 @@ public class CommandMechanic implements Listener {
                 public void execute(CommandSender sender, String[] args) {
                     ProxiedPlayer player = (ProxiedPlayer) sender;
                     if(!player.getServer().getInfo().getMotd().equals("Lobby-1")){
-                        ChatNotice.success(player, Component.text("Přesunušeš se na Lobby."));
+                        ChatNotice.success(player, Component.text("Připojuješ se na Lobby."));
                         player.connect(ProxyServer.getInstance().getServerInfo("Lobby-1"));
                         return;
                     }
